28474854b1e8e6b80b4925384ebea12be6467918
Alexey Piltenko <ukr.top@ukr.net>
2022-05-04 17:11:52 +0300

    1st step

---
+++ index.js
@@ -0,0 +1,5 @@
+let request = require('request');
+let url =
+  'https://api.memegen.link/images/bad/your_meme_is_bad/and_you_should_feel_bad.jpg?width=300';
+
+request(url, function (err, response, body) {});
