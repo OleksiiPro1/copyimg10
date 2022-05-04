4ecb886c1f5bf8e80ff91eab309ebd73f95c150d
Alexey Piltenko <ukr.top@ukr.net>
2022-05-04 16:27:27 +0300

    2 variant

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-text
+Try 1:
+let scrape = require('scrape');
+scrape.request(
+'https://memegen-link-examples-upleveled.netlify.app/',
+function (err, $) {
+if (err) return console.error(err);
+
+    $('div.link').each(function (div) {
+      let score = div.find('div.score.unvoted').first(),
+        link = div.find('a.title').first();
+      console.log('[%s] %s (%s)', score.text, link.text, link.attribs.href);
+    });
+
+},
+);
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('abc');
+let scrape = require('scrape');
+scrape.request(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+  function (err, $) {
+  if (err) return console.error(err);
+
+      $('div.link').each(function (div) {
+        let score = div.find('div.score.unvoted').first(),
+          link = div.find('a.title').first();
+        console.log('[%s] %s (%s)', score.text, link.text, link.attribs.href);
+      });
+
+  },
+  );
+let scrape = require('scrape');
+let urls = [...];
+
+scrape.concurrent(urls, 10, function (url, next) {
+    if (url) {
+        scrape.request(url, function (err, $) {
+
+            next();
+        });
+    } else {
+
+    }
+});
