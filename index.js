b27ed3820075bb6cf7b844752e1ae2f056c81766
Alexey Piltenko <ukr.top@ukr.net>
2022-05-05 16:25:10 +0300

    10 pics

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 import axios from 'axios';
 
-const res = await axios.get('https://api.memegen.link/images/');
-console.log(res);
+// Папка с картинками https://api.memegen.link/images/
 
-let getImageUrls = require('get-image-urls');
+const res = await axios.get('https://api.memegen.link/images/');
+console.log(res.data[0].url);
+console.log(res.data[1].url);
+console.log(res.data[2].url);
+console.log(res.data[3].url);
+console.log(res.data[4].url);
+console.log(res.data[5].url);
+console.log(res.data[6].url);
+console.log(res.data[7].url);
+console.log(res.data[8].url);
+console.log(res.data[9].url);
